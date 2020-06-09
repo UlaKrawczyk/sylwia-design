75ce6c2b37cf82ad1837fbc86ca627128e1d55d2
Ula Krawczyk <ula.krawczyk7@gmail.com>
2020-06-10 01:25:46 +0200

    feat: section reviews with slider in js

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import '../scss/main.scss';
 
 /* place your code below */
 
+//section features fades in and section tips slides in
 const faders = document.querySelectorAll('.fade-in');
 const sliders = document.querySelectorAll('.slide-in');
 
@@ -28,3 +29,52 @@ faders.forEach(fader => {
 sliders.forEach(slider => {
   appearOnScroll.observe(slider);
 });
+
+//section reviews slider + dots changing color
+//we need as many dots as reviews!
+const sliderImages = document.querySelectorAll('.reviews__slide');
+const dots = document.querySelectorAll('.reviews__span');
+const arrowLeft = document.querySelector("#arrow-left");
+const arrowRight = document.querySelector("#arrow-right");
+let current = 0;
+
+function reset() {
+  for(let i = 0; i<sliderImages.length; i++) {
+    sliderImages[i].style.display = 'none';
+    dots[i].style.color = '#160441';
+  }
+}
+
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = 'block';
+  dots[0].style.color = "#536c59";
+}
+
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = 'block';
+  dots[current - 1].style.color = '#536c59';
+  current--;
+}
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = 'block';
+  dots[current + 1].style.color = '#536c59';
+  current++;
+}
+
+arrowLeft.addEventListener('click', function() {
+  if(current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
+arrowRight.addEventListener('click', function() {
+  if(current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+
+startSlide();
