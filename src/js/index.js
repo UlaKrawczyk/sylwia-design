da6e9585348324bab4f8c9c634490e2f304716f0
Ula Krawczyk <ula.krawczyk7@gmail.com>
2020-06-09 22:38:26 +0200

    feat: section features fade-in animation, section tips slide-in animation

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,29 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
+
+
+const appearOptions = {
+  rootMargin: '0px 0px -100px 0px',
+  threshold: 0
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
